6b2de992eae16fa9ad2d428ff85cf9da60c7fcf9
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-05-23 12:56:30 -0400

    Hardcoding to default IP of 192.168.1.23

--- src/hooks/useEmailValidation.tsx
+++ src/hooks/useEmailValidation.tsx
@@ -33,7 +33,7 @@ export function useEmailValidation(optionalCallback: any = noop) {
         }
       }
 
-      postData('http://192.168.0.104:8080/start', txn)
+      postData('http://192.168.1.23:8080/start', txn)
         .then(data => {
           // console.log("API response");
           // console.log(data); // JSON data parsed by `response.json()` call
