5ec4069bda5b4e893f780a0b7f5163e14a77589d
Waqas Ahmed <wakqasahmed@gmail.com>
2020-07-01 23:46:06 +0400

    Removed comments

--- src/hooks/useDID.tsx
+++ src/hooks/useDID.tsx
@@ -7,10 +7,8 @@ export function useDID(optionalCallback: any = noop) {
      * Request some credentials to the DID application.
      */
     appManager.sendIntent("credaccess", { claims }, {}, (response: any) => {
-      console.log("Credential access response received", response)
 
       if (response && response.result && response.result.presentation) {
-        console.log("Received a presentation, so we are now signed in.", response.result);
 
         // Create a real presentation object from json data
         didManager.VerifiablePresentationBuilder.fromJson(JSON.stringify(response.result.presentation), (presentation)=>{
